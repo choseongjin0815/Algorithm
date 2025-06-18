49d58bd79371ec2ae2c681d5c0534e2fc17b80d2
choseongjin0815 <chobocho0815@gmail.com>
2025-06-18 20:39:54 +0900

    알고리즘 문제

---
+++ BOJ/src/BOJ_11382.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long k = sc.nextLong();
+
+        System.out.println(n+m+k);
+    }
+}
